9db234f9dce956b168e3fba539f889aba183ada6
Marko Boger <marko.boger@htwg-konstanz.de>
2013-11-28 15:22:33 +0100

    GameCreator compiles

--- de.htwg.sa.sudoku/src/de/htwg/sudoku/controller/impl/GameCreator.java
+++ de.htwg.sa.sudoku/src/de/htwg/sudoku/controller/impl/GameCreator.java
@@ -1,22 +1,36 @@
 package de.htwg.sudoku.controller.impl;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import com.google.inject.Inject;
+
 import de.htwg.sudoku.controller.ISudokuController;
+import de.htwg.sudoku.model.Step;
+import de.htwg.sudoku.solverplugin.SolverPlugin;
 
 public class GameCreator {
 	
-	public static boolean findHardGame(ISudokuController controller) {
+	@Inject
+	public static boolean findHardGame(ISudokuController controller, Set<SolverPlugin> plugins) {
 		boolean hardGameFound = false;
+		Step nextStep = null;
 		while (!hardGameFound) {
 			controller.create();
 			boolean gameSolved=false;
 			while(!gameSolved) {
-				if (controller.findSolveStep()) { 
-					controller.doSolveStep()
+				Iterator<SolverPlugin> iter = plugins.iterator();
+				while (iter.hasNext()) {
+					final SolverPlugin plugin = iter.next();
+					nextStep = plugin.solveStep(controller);
+				}
+				if (nextStep!=null) { 
+					controller.setValue(nextStep.getRow(), nextStep.getColumn(), nextStep.getValue());
 				} else {
 					hardGameFound = true;
 					return hardGameFound;
 				}
-				gameSolved=controller.isSolved()
+				gameSolved=controller.isSolved();
 			}
 			
 		}
@@ -24,3 +38,4 @@ public class GameCreator {
 	}
 
 }
+
