c8b93f94a94e74aa973a7fd4d3797059db3c2411
Marko Boger <marko.boger@gmail.com>
2013-11-28 13:15:13 +0100

    added draft to find hard games

---
+++ de.htwg.sa.sudoku/src/de/htwg/sudoku/controller/impl/GameCreator.java
@@ -0,0 +1,26 @@
+package de.htwg.sudoku.controller.impl;
+
+import de.htwg.sudoku.controller.ISudokuController;
+
+public class GameCreator {
+	
+	public static boolean findHardGame(ISudokuController controller) {
+		boolean hardGameFound = false;
+		while (!hardGameFound) {
+			controller.create();
+			boolean gameSolved=false;
+			while(!gameSolved) {
+				if (controller.findSolveStep()) { 
+					controller.doSolveStep()
+				} else {
+					hardGameFound = true;
+					return hardGameFound;
+				}
+				gameSolved=controller.isSolved()
+			}
+			
+		}
+		return hardGameFound;
+	}
+
+}
